dcdebb003ce98dc334000a20547becd37b0dc435
Brian Bolli <bbolli@binarypursuits>
2022-01-24 19:14:55 -0600

    added default value

--- src/components/HelloWorld/HelloWorld.tsx
+++ src/components/HelloWorld/HelloWorld.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { HelloWorldProps } from './HelloWorldProps';
 
-export default function HelloWorld ({ name }: HelloWorldProps) {
+export default function HelloWorld ({ name = 'John Doe' }: HelloWorldProps) {
     return <h1>Hello World from { name }</h1>;
 }
